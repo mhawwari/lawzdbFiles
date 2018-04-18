9e35ef93865347960e0c4e5e828169f922b8ec3c
mhawwari <hawwari80@gmail.com>
2018-04-18 12:30:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
+	header('Location: '.$uri.'/android_login_api/');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
