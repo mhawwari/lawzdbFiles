7acd4a99263bd32f90cafddb246f805ed6a805cb
mhawwari <hawwari80@gmail.com>
2018-04-18 12:38:13 +0300

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohammad Hawwari
+ * Date: 4/4/2018
+ * Time: 6:04 PM
+ */
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "den1.mysql2.gear.host");
+define("DB_USER", "lawzdb");
+define("DB_PASSWORD", "LAWZ!@#$");
+define("DB_DATABASE", "lawzdb");
