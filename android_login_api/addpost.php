1047f27923ff87d838031e96f0727a3591aec984
mhawwari <hawwari80@gmail.com>
2018-05-16 23:01:04 +0300

    Update addpost.php

--- android_login_api/addpost.php
+++ android_login_api/addpost.php
@@ -8,10 +8,17 @@
 
 require_once 'include/DB_Community_Functions.php';
 $db = new DB_Community_Functions();
+//connect to cloud server
+$ftp_server = "ftp.gear.host";
+$ftp_username = "lawscloud\$lawscloud"
+$ftp_userpass = "fio9bNvaDhtW7BHl92PXo6rRdXGfPmcag1xr4c24kWoPdjKyk8EMyeCjPiL4"
+$ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
+$login = ftp_login($ftp_conn, $ftp_username, $ftp_userpass);
+$destination_path = "site/repository/img/"; 
 
 // json response array
 $response = array("error" => FALSE);
-define('UPLOAD_PATH', "site/repository/img/");
+define('UPLOAD_PATH', "https://lawscloud.gearhostpreview.com/img/");
 
 if (isset($_POST['title']) && isset($_POST['content']) && isset($_POST['user_id']) && isset($_POST['topic'])) {
 
@@ -24,7 +31,8 @@ if (isset($_POST['title']) && isset($_POST['content']) && isset($_POST['user_id'
     if (isset($_FILES['pic']['name'])) {
         try
         {
-            move_uploaded_file($_FILES['pic']['name'], UPLOAD_PATH . $_FILES['pic']['name']);
+            $upload = ftp_put($ftp_conn, $destination_path.$_FILES['pic']['name'], $_FILES['pic']['tmp_name'], FTP_BINARY);
+            //move_uploaded_file($_FILES['pic']['name'], UPLOAD_PATH . $_FILES['pic']['name']);
             $image = UPLOAD_PATH.$_FILES['pic']['name'];
         }
         catch (Exception $e){
