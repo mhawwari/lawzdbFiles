a806baaab5f5de0b7a9052800363bf0499457e0c
mhawwari <hawwari80@gmail.com>
2018-05-16 23:19:21 +0300

    Update addpost.php

--- android_login_api/addpost.php
+++ android_login_api/addpost.php
@@ -16,12 +16,6 @@ $ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
 $login = ftp_login($ftp_conn, $ftp_username, $ftp_userpass);
 $destination_path = "site/repository/img/"; 
 
-if ($login || $ftp_connect)
-{
-    $response['error'] = true;
-            $response['message'] = 'Could ';
-            echo json_encode($response);
-}
 // json response array
 $response = array("error" => FALSE);
 define('UPLOAD_PATH', "http://lawscloud.gearhostpreview.com/img/");
