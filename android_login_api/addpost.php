925bb528d0385f5992009eba9cc77b328cf477e0
mhawwari <hawwari80@gmail.com>
2018-05-17 16:15:31 +0300

    Update addpost.php

--- android_login_api/addpost.php
+++ android_login_api/addpost.php
@@ -16,7 +16,7 @@ $ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
 $login = ftp_login($ftp_conn, $ftp_username, $ftp_userpass);
 $destination_path = "site/repository/img/"; 
 
-if ($login || $ftp_connect)
+if (!$login || !$ftp_connect)
 {
     $response['error'] = true;
             $response['message'] = 'Could ';
